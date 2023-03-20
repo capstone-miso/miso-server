bf04338fc27df0f2e68a80114ba343feaf908fc8
duckbillLvr <uhyeon7399@daum.net>
2023-03-20 17:09:00 +0900

    data collect

---
+++ dishcovery-data/src/main/java/capstonedishcovery/data/DishcoveryDataApplication.java
@@ -0,0 +1,13 @@
+package capstonedishcovery.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishcoveryDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishcoveryDataApplication.class, args);
+    }
+
+}
---
+++ dishcovery-data/src/test/java/capstonedishcovery/data/DishcoveryDataApplicationTests.java
@@ -0,0 +1,13 @@
+package capstonedishcovery.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishcoveryDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
